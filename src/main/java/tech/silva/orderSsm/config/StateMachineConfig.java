10fae181c6e7b2b57c3e6fbd1d8f4c7f0fe9be82
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-01 13:37:28 -0300

    feat: configure states

--- src/main/java/tech/silva/orderSsm/config/StateMachineConfig.java
+++ src/main/java/tech/silva/orderSsm/config/StateMachineConfig.java
@@ -3,11 +3,25 @@ package tech.silva.orderSsm.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigBuilder;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import tech.silva.orderSsm.entity.OrderEvents;
+import tech.silva.orderSsm.entity.OrderStates;
+
+import java.util.EnumSet;
 
 @Configuration
 @EnableStateMachineFactory
-public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<> {
+public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<OrderStates, OrderEvents> {
 
+    @Override
+    public void configure(StateMachineStateConfigurer<OrderStates, OrderEvents> states) throws Exception {
+        states
+                .withStates()
+                .initial(OrderStates.NEW)
+                .states(EnumSet.allOf(OrderStates.class))
+                .end(OrderStates.COMPLETED)
+                .end(OrderStates.CANCELLED);
 
-
+    }
 }
