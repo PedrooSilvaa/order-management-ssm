30a8798edcb5c0210be5dd4db4aadd8b4e761551
PedroHenrique2904 <pedrohenriquefdasilva14@gmail.com>
2024-09-01 13:19:27 -0300

    feat: create project

---
+++ src/main/java/tech/silva/orderSsm/OrderManagmentSsmApplication.java
@@ -0,0 +1,13 @@
+package tech.silva.orderSsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagmentSsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagmentSsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/silva/orderSsm/OrderManagmentSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.silva.orderSsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagmentSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
